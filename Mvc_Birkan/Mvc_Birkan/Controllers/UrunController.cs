c167ed4ecf5f431e07dfac022acf80d4134dbbdf
Unknown <birkankose95@gmail.com>
2017-12-11 17:08:52 +0300

    Edit Eklendi

--- Mvc_Birkan/Mvc_Birkan/Controllers/UrunController.cs
+++ Mvc_Birkan/Mvc_Birkan/Controllers/UrunController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -45,6 +46,54 @@ namespace Mvc_Birkan.Controllers
             ctx.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Urunler urunler = ctx.Urunlers.Find(id);
+            if (urunler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KategoriID = new SelectList(ctx.Kategorilers, "KategoriID", "KategoriAdi", urunler.KategoriID);
+            ViewBag.TedarikciID = new SelectList(ctx.Tedarikcilers, "TedarikciID", "SirketAdi", urunler.TedarikciID);
+            return View(urunler);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UrunID,UrunAdi,TedarikciID,KategoriID,BirimdekiMiktar,Fiyat,Stok,YeniSatis,EnAzYenidenSatisMikatari,Sonlandi")] Urunler urunler)
+        {
+            if (ModelState.IsValid)
+            {
+                var Entity = ctx.Urunlers.Find(urunler.UrunID);
+                if (Entity != null)
+                {
+                    Entity.UrunAdi = urunler.UrunAdi;
+                    Entity.TedarikciID = urunler.TedarikciID;
+                    Entity.KategoriID = urunler.KategoriID;
+                    Entity.BirimdekiMiktar = urunler.BirimdekiMiktar;
+                    Entity.Fiyat = urunler.Fiyat;
+                    Entity.Stok = urunler.Stok;
+                    Entity.YeniSatis = urunler.YeniSatis;
+                    Entity.EnAzYenidenSatisMikatari = urunler.EnAzYenidenSatisMikatari;
+                    Entity.Sonlandi = urunler.Sonlandi;
+                    ctx.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(urunler);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ctx.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
 
     }
 }
